f7ce436b5378be96404dad614ce920a9d859828b
Nina Tran <nana@MacBook-Air-525.local>
2025-10-17 02:42:15 -0500

    add nav bar and visit page

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import { Route, Routes } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
 import Footer from './components/Footer'
+import Visit from './components/Visit'
 
 function App() {
   return (
     <div>
-      <h1>Museum To Be Continued</h1>
+
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/visit" element={<Visit />} />
         <Route path="/artworks" element={<Artwork />} />
         <Route path="/artworks/:id" element={<ArtworkDetail />} />
       </Routes>
